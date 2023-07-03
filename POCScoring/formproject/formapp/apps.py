ce396adc02b0c607a155ee371108512e49518aa6
smperumalla <138521051+smperumalla@users.noreply.github.com>
2023-07-03 11:32:46 -0700

    Cloned project into repository

---
+++ POCScoring/formproject/formapp/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+class FormappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'formapp'
+
+    def ready(self):
+        import formapp.signals  # noqa
+
